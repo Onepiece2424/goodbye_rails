5c9bd98206d577329e08b8f7565100daa6642cb9
rioto.nagano <kingdom950927@gmail.com>
2023-04-01 11:06:26 +0900

    sampleテキストの内容をeach_lineを用いて出力

--- sampletxt_read.rb
+++ sampletxt_read.rb
@@ -1,3 +1,11 @@
+# sampleテキストの内容を出力
 File.open("sample.txt", "r") do |f|
   puts f.read
+end
+
+# sampleテキストの内容をeach_lineを用いて出力
+File.open("sample.txt", "r") do |f|
+  f.each_line do |line|
+    puts line
+  end
 end
