a0f0120e12eb4f3e90918123a9ab7eb3d25ad163
rioto.nagano <kingdom950927@gmail.com>
2023-04-01 12:27:34 +0900

    コメントの追加

--- student_length.rb
+++ student_length.rb
@@ -8,7 +8,7 @@ hash_data = JSON.parse(file)
 # 身長順に並び替え
 height_order_reulst = hash_data["student"].sort_by { |s| s["height"] }
 
-# 一番身長が大きいの名前を出力
+# 一番身長が大きいの名前を出力（height_top = height_order_reulst.first["name"]でも良い）
 height_top = height_order_reulst[0]["name"]
 
 puts height_top
