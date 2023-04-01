dd33d69bbcbb55399463778d74a4c13d891c9ffa
rioto.nagano <kingdom950927@gmail.com>
2023-04-01 12:26:39 +0900

    firstメソッドを[0]に変更

--- student_length.rb
+++ student_length.rb
@@ -9,6 +9,6 @@ hash_data = JSON.parse(file)
 height_order_reulst = hash_data["student"].sort_by { |s| s["height"] }
 
 # 一番身長が大きいの名前を出力
-height_top = height_order_reulst.first["name"]
+height_top = height_order_reulst[0]["name"]
 
 puts height_top
