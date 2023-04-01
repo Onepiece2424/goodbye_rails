a83226a7d13cad5d3cc5a3bc79ec4fcdd29f533e
rioto.nagano <kingdom950927@gmail.com>
2023-04-01 10:40:47 +0900

    不要な部分をコメントアウト

--- costdata_read.rb
+++ costdata_read.rb
@@ -1,12 +1,13 @@
 require "json"
+require "pry-byebug"
 
 # JSONデータからvalueのみを取得（方法①）
-json_data = File.read("./costdata.json")
-hash_data = JSON.parse(json_data)
+# json_data = File.read("./costdata.json")
+# hash_data = JSON.parse(json_data)
 
-hash_data.each do |key, value|
-  p value
-end
+# hash_data.each do |key, value|
+#   p value
+# end
 
 # JSONデータからvalueのみを取得（方法②）
 File.open("./costdata.json") do |file|
