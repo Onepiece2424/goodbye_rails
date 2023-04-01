c44d4ece3b325feb972f9cab2a39798fde25aa8a
rioto.nagano <kingdom950927@gmail.com>
2023-04-01 12:11:44 +0900

    JSONデータを身長が一番大きい人のデータを出力

---
+++ student_length.rb
@@ -0,0 +1,14 @@
+require "json"
+require "pry-byebug"
+
+# JSONデータをハッシュに変換
+file = File.read("student.json")
+hash_data = JSON.parse(file)
+
+# 身長順に並び替え
+height_order_reulst = hash_data["student"].sort_by { |s| s["height"] }
+
+# 一番身長が大きいの名前を出力
+height_top = height_order_reulst.first["name"]
+
+puts height_top
